35b705b47fe130c9413191477760be8518b21e4c
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-05-01 16:48:52 +0200

    ajouté champs select dans view + validations dans table event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+    TYPE = %w(Saison Tournoi)
     belongs_to :club
+    validates :name, presence: true, uniqueness: true
+    validates :type, presence: true, uniqueness: true
+    validates :type, inclusion: { in: TYPE}
 end
