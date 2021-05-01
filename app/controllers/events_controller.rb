817f8fab5abe0dfdf432ed01d34c6e3907f3a26a
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-05-01 17:20:36 +0200

    ajouté validation select et changé nom de la colonne en event_type

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,6 +71,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:name)
+      params.require(:event).permit(:name, :event_type)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,10 @@
 class Event < ApplicationRecord
     TYPE = %w(Saison Tournoi)
     belongs_to :club
-    validates :name, presence: true, uniqueness: true
-    validates :type, presence: true, uniqueness: true
-    validates :type, inclusion: { in: TYPE}
+    validates :name, presence: true
+    # validates :winning_points, presence: true
+    # validates :loosing_points, presence: true
+    # validates :presence_points , presence: true
+    # validates :draw_points , presence: true
+    validates :event_type, inclusion: { in: TYPE}, presence: true
 end
