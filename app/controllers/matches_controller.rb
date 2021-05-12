126c5221a6803a98495468b7fc1ed57ab1b4795f
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-05-12 10:49:33 +0200

    changed link + update method

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -43,7 +43,7 @@ class MatchesController < ApplicationController
   def update
     respond_to do |format|
       if @match.update!(match_params)
-        format.html { redirect_to @match, notice: "Match was successfully updated." }
+        format.html { redirect_to club_event_series_path, notice: "Match was successfully updated." }
         format.json { render :show, status: :ok, location: @match }
       else
         format.html { render :edit, status: :unprocessable_entity }
