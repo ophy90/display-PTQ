dda558cbaf8a7e83ef09454cdcfc25729ef86278
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-05-14 14:25:20 +0200

    added 2 seeds for club + 2 seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts " Destroying clubs"
+Club.destroy_all
+puts "Clubs destroyed"
+puts " Creating 2 clubs..."
+
+Club.create!(name: "Torny Boules")
+Club.create!(name: "Farvagny Sharks")
+
+puts "Clubs have been succesfully created!"
+
+puts " Destroying users"
+User.destroy_all
+puts "Users destroyed"
+puts " Creating 2 Users..."
+
+User.create!(email: "ophelie@ophelie.ch", password: "123456", club_id: 1, role: "admin")
+User.create!(email: "yoann@yoann.ch", password: "123456", club_id: 2, role: "user")
+
+puts "Users have been succesfully created!"
