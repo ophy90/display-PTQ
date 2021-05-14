6b87d71c2a96494a9e28b0be99aeb861fd0f2e32
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-05-14 14:13:59 +0200

    added column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_094642) do
+ActiveRecord::Schema.define(version: 2021_05_14_095528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2021_05_14_094642) do
     t.string "role"
     t.integer "total_shots"
     t.integer "successful_shots"
+    t.bigint "club_id", null: false
+    t.index ["club_id"], name: "index_users_on_club_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -82,4 +84,5 @@ ActiveRecord::Schema.define(version: 2021_05_14_094642) do
   add_foreign_key "events", "clubs"
   add_foreign_key "matches", "series", column: "serie_id"
   add_foreign_key "series", "events"
+  add_foreign_key "users", "clubs"
 end
