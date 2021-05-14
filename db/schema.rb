5325ab38b96a3aa97481eed5806455ea0600936f
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-05-14 11:11:38 +0200

    supprimé table users avec rails g drop_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_000152) do
+ActiveRecord::Schema.define(version: 2021_05_14_090846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,14 +62,6 @@ ActiveRecord::Schema.define(version: 2021_05_06_000152) do
     t.integer "duration"
     t.bigint "event_id", null: false
     t.index ["event_id"], name: "index_series_on_event_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "role"
-    t.integer "total_shots"
-    t.string "succesful_shots"
   end
 
   add_foreign_key "events", "clubs"
